0f5b7ba8bd9e2d08ede3d6ebb8a9d5453f2b408c
Darel13712 <darel142857@gmail.con>
2018-11-25 16:40:57 +0300

    initial commit 2

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+
+setup(name='global_features',
+      version='0.1',
+      description='Tool to get extra features from open sources',
+      url='http://github.com/Darel13712/global_features'
+      )
